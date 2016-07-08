56f1be9edaffe47d7924e7c007b526187be4dbe1
残月 <632342527@qq.com>
2016-07-08 19:55:55 +0800

    base64AndSSKey
    base64和钥匙串

--- base64和钥匙串/ViewController.m
+++ base64和钥匙串/ViewController.m
@@ -7,6 +7,7 @@
 //
 
 #import "ViewController.h"
+#import "SSKeychain.h"
 
 @interface ViewController ()
 
@@ -15,9 +16,53 @@
 @implementation ViewController
 
 - (void)viewDidLoad {
+    
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    NSString *str = @"http:///login.php";
+    NSURL *url = [NSURL URLWithString:str];
+    
+    NSMutableURLRequest *request = [[NSMutableURLRequest alloc]initWithURL:url];
+    NSString *username =[SSKeychain passwordForService:[NSBundle mainBundle].bundleIdentifier account:@"username"];
+    NSString *password =[SSKeychain passwordForService:[NSBundle mainBundle].bundleIdentifier account:@"password"];
+    if (username==nil&&password==nil) {
+        username = @"张 张";
+        
+        
+        
+        NSData *data = [username dataUsingEncoding:NSUTF8StringEncoding];
+        username = [data base64EncodedStringWithOptions:0];
+        
+        
+        password = @"saner";
+        NSData *data2 = [password dataUsingEncoding:NSUTF8StringEncoding];
+        password = [data2 base64EncodedStringWithOptions:0];
+        
+        [SSKeychain setPassword:username forService:[NSBundle mainBundle].bundleIdentifier account:@"username"];
+        [SSKeychain setPassword:password forService:[NSBundle mainBundle].bundleIdentifier account:@"password"];
+        
+        NSLog(@"%@------%@",username,password);
+        
+    }
+//    
+//    [SSKeychain deletePasswordForService:[NSBundle mainBundle].bundleIdentifier account:@"username"];
+//    
+//    [SSKeychain deletePasswordForService:[NSBundle mainBundle].bundleIdentifier account:@"password"];
+    
+    request.HTTPMethod = @"POST";
+    NSString *para = [NSString stringWithFormat:@"username=%@&password=%@",username,password];
+    
+    request.HTTPBody =[para dataUsingEncoding:NSUTF8StringEncoding];
+    
+    
+    [NSURLConnection sendAsynchronousRequest:request queue:[NSOperationQueue mainQueue] completionHandler:^(NSURLResponse * _Nullable response, NSData * _Nullable data, NSError * _Nullable connectionError) {
+        
+        NSDictionary *dict = [NSJSONSerialization JSONObjectWithData:data options:0 error:nil];
+        NSLog(@"%@",dict);
+    
+    }];
+    
 }
+
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
